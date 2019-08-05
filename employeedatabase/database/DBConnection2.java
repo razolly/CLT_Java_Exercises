d30b91c22f61eeeda4e29db3739d25b205424991
razli.mahadi <razli.mahadi@TRAINING019.optimum.net>
2019-08-05 14:37:11 +0800

    Setting up JDBC Connection with properties from text file

---
+++ employeedatabase/database/DBConnection2.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection2 {
+
+	public static Connection myConnection() {
+		
+		Connection con = null;
+		FileReader reader = null;
+		Properties prop = new Properties();
+		
+		try {
+			reader = new FileReader("db.properties");
+			
+			// Load JDBC related properties in above file
+			prop.load(reader);
+			
+			// Load the driver class
+			Class.forName(prop.getProperty("db_driver_class"));
+			
+			// Create the connection
+			con = DriverManager.getConnection(
+					prop.getProperty("db_url"),
+					prop.getProperty("db_username"),
+					prop.getProperty("db_password"));
+			
+		} catch (IOException | ClassNotFoundException | SQLException e) {
+			System.out.println("DB Connection Error!");
+		}
+		
+		return con;
+		
+	}
+	
+}
